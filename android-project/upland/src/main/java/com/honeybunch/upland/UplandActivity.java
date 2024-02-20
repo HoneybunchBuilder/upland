fe09bcf0f3c6f3f95b88c9ec6ea2cfc7f7735391
Honeybunch <amt3824@g.rit.edu>
2024-02-19 21:10:24 -0800

    Adding android project

---
+++ android-project/upland/src/main/java/com/honeybunch/upland/UplandActivity.java
@@ -0,0 +1,11 @@
+package com.honeybunch.upland;
+
+import org.libsdl.app.SDLActivity;
+
+public class UplandActivity extends SDLActivity {
+    protected String[] getLibraries() {
+        return new String[] {
+                "upland",
+        };
+    }
+};
